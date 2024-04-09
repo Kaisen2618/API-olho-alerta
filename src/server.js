06a29acfe42773d48397b270c073b79ce3ced117
Kaisen2618 <159200328+Kaisen2618@users.noreply.github.com>
2024-04-09 20:53:08 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -2,12 +2,17 @@
 const express = require('express');
 const denunciasRouter = require('./routes/denuncias')
 
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require ('./swagger_output.json')
+
 const app = express()
 
 app.use(express.json())
+
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use('/denuncias', denunciasRouter)
 
 // Conexão ao banco de dados
 app.listen(3333,
-     () => console.log('⚡Servidor ligado http://:localhost3333⚡'))
+     () => console.log('⚡Servidor ligado http://:localhost:3333⚡'))
