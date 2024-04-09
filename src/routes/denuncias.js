dc41393f560d229d0dbd2bea25021204911e55e3
Kaisen2618 <159200328+Kaisen2618@users.noreply.github.com>
2024-04-08 21:34:05 -0300

    Add files via upload

--- src/routes/denuncias.js
+++ src/routes/denuncias.js
@@ -1,7 +1,7 @@
+//importando as bibliotecas
 const express = require('express')
 const prisma = require('../lib/prisma')
 const {z} = require('zod') 
-
 
 const router = express.Router()
 
@@ -32,6 +32,24 @@ router.post('/', async (req, res) => {
     }catch (error) {
         
         res.status(400).json({error:'Dados invalidos no corpo da solicitação'})
+    }
+})
+
+//rota para oter todas as denuncias
+router.get('/', async (req, res) => {
+    try{
+    //Buscar todas as denuncias no banco de dados usando o prisma
+    const denuncias = await prisma.denuncia.findMany()
+
+    //verificar se há denuncias
+    if(denuncias.length === 0){
+        return res.status(404).json({message:'Nenhuma denuncia encontrada'})
+    }
+
+    //retornar as denuncias como resposta no formato JSON
+    res.json(denuncias)
+    }catch (error) {
+        res.status(500).json({error:'Erro ao recuperar as denuncias'})
     }
 })
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
+//importando as bibliotecas
 const express = require('express');
 const denunciasRouter = require('./routes/denuncias')
 
 const app = express()
 
+app.use(express.json())
+
 app.use('/denuncias', denunciasRouter)
 
+// Conexão ao banco de dados
 app.listen(3333,
      () => console.log('⚡Servidor ligado http://:localhost3333⚡'))
